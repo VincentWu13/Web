8f43088c582fed86445369f607b98ba059373ea2
gfat813 <awpwwch@163.com>
2019-03-31 11:10:12 +0800

    first upload

---
+++ manage.py
@@ -0,0 +1,39 @@
+import os
+
+import flask
+from flask import render_template, send_file
+
+app = flask.Flask('Vincent', template_folder='html')
+
+
+@app.route('/')
+def homepage():
+    return render_template(
+        'index.html')
+
+
+@app.route('/index.html')
+def homepage_2():
+    return render_template(
+        'index.html')
+
+
+@app.route('/diary_1.html')
+def diary_1():
+    return render_template(
+        'diary_1.html')
+
+
+@app.route('/diary_2.html')
+def diary_2():
+    return render_template(
+        'diary_2.html')
+
+
+@app.route('/<path:path>')
+def return_file(path):
+    return send_file(path)
+
+
+if __name__ == "__main__":
+    app.run()
